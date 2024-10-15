2d1bf548b09358fb08aae8c85e989a904ce13b53
LilWinx <lil.winx@gmail.com>
2024-10-15 19:49:52 +1100

    fix discrepant assembly vs mapping for 23s rRNA

--- scripts/mres_map.py
+++ scripts/mres_map.py
@@ -52,29 +52,31 @@ def vcf_calc_and_blast_match(bcftools_vcf, mutation_list):
     vcf_df = pd.read_csv(StringIO(oneline), sep="\t", header=0)
 
     info_data = vcf_df["INFO"].str.split(";", expand = True)
-    mres_df = pd.DataFrame()
-    mres_df['DP4'] = info_data.apply(lambda row: next((cell for cell in row if "DP4=" in cell), None), axis=1).str.lstrip('DP4=')
-    split_columns = mres_df['DP4'].str.split(',', expand=True)
-    split_columns = split_columns.apply(pd.to_numeric, errors='coerce')
-    mres_df['sum_all_four'] = split_columns.sum(axis=1).astype(int)
-    mres_df['sum_last_two'] = split_columns.iloc[:, -2:].sum(axis=1).astype(int)
-    mres_df['FREQ'] = mres_df['sum_last_two'] / mres_df['sum_all_four']
+    if not info_data.empty:
+        mres_df = pd.DataFrame()
+        mres_df['DP4'] = info_data.apply(lambda row: next((cell for cell in row if "DP4=" in cell), None), axis=1).str.lstrip('DP4=')
+        split_columns = mres_df['DP4'].str.split(',', expand=True)
+        split_columns = split_columns.apply(pd.to_numeric, errors='coerce')
+        mres_df['sum_all_four'] = split_columns.sum(axis=1).astype(int)
+        mres_df['sum_last_two'] = split_columns.iloc[:, -2:].sum(axis=1).astype(int)
+        mres_df['FREQ'] = mres_df['sum_last_two'] / mres_df['sum_all_four']
 
-    # extract only V domain
-    vdomain_start, vdomain_end = 1918, 2444
-    mres_df["REFPOSALT"], mres_df["POS"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"], vcf_df["POS"]
-    mres_df = mres_df[(mres_df["POS"] >= vdomain_start) & (mres_df["POS"] <= vdomain_end)]
-    mres_df["REFPOSALT"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"]
-    
-    copy_no = mres_df["FREQ"].apply(determine_copy_number)
-    final_df = mres_df[['REFPOSALT', 'FREQ']]
-    mask = final_df['REFPOSALT'].isin(mutation_list)
-    mres_df = mres_df[mask].reset_index()
-    if not mres_df.empty:
-        positions = ",".join(mutation_list)
-        copy_no = mres_df["FREQ"].apply(determine_copy_number)[0]
-        logging.info(f"23s mutation occurs as a {positions}, very likely in {copy_no}")
+        # extract only V domain
+        vdomain_start, vdomain_end = 1918, 2444
+        mres_df["REFPOSALT"], mres_df["POS"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"], vcf_df["POS"]
+        mres_df = mres_df[(mres_df["POS"] >= vdomain_start) & (mres_df["POS"] <= vdomain_end)]
+        mres_df["REFPOSALT"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"]
+        
+        copy_no = mres_df["FREQ"].apply(determine_copy_number)
+        final_df = mres_df[['REFPOSALT', 'FREQ']]
+        mask = final_df['REFPOSALT'].isin(mutation_list)
+        mres_df = mres_df[mask].reset_index()
+        if not mres_df.empty:
+            positions = ",".join(mutation_list)
+            copy_no = mres_df["FREQ"].apply(determine_copy_number)[0]
+            logging.info(f"23s mutation occurs as a {positions}, very likely in {copy_no}")
     else:
+        final_df = pd.DataFrame()
         logging.info(f"Mutations detected in assembly was not detected in mapping.")
     
     if "A2037G" in mutation_list:

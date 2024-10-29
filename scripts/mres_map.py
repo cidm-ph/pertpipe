6e5cac58972784e63e5217746ed6c08823f0b9b6
LilWinx <lil.winx@gmail.com>
2024-10-29 11:19:16 +1100

    add coverage check for 23S mutation checking

--- scripts/mres_map.py
+++ scripts/mres_map.py
@@ -5,8 +5,10 @@ from io import StringIO
 from scripts import assists
 
 rrna_seq = os.path.join(os.path.dirname(os.path.dirname(__file__)), "databases/23S_rRNA.fasta")
+rrna_bed = os.path.join(os.path.dirname(os.path.dirname(__file__)), "databases/23s_rRNA_VDomain.bed")
 ref_list = os.path.join(os.path.dirname(os.path.dirname(__file__)), "databases/silva_bor.bed")
 kallisto_db = os.path.join(os.path.dirname(os.path.dirname(__file__)), "databases/SILVA_138.2_LSURef_NR99")
+
 
 def mres_map(reads1, reads2, outdir, mutation_list, meta):
     if meta is True:
@@ -25,22 +27,29 @@ def mres_map(reads1, reads2, outdir, mutation_list, meta):
     assists.run_cmd(minimap2_cmd)
     samtools_sort_cmd = f"samtools sort {outdir}/23s_aln.bam > {outdir}/23s_aln.sort.bam"
     samtools_index_cmd = f"samtools index {outdir}/23s_aln.sort.bam"
-    assists.run_cmd(samtools_sort_cmd)
-    assists.run_cmd(samtools_index_cmd)
+    samtools_depth_cmd = f"samtools depth -a {outdir}/23s_aln.sort.bam -b {rrna_bed} > {outdir}/depth.txt"
+    for cmd in [samtools_sort_cmd, samtools_index_cmd, samtools_depth_cmd]:
+        assists.run_cmd(cmd)
+    if os.path.exists(f"{outdir}/depth.txt") is True and os.stat(f"{outdir}/depth.txt").st_size != 0:
+        depth_df = pd.read_csv(f"{outdir}/depth.txt", sep="\t")
+        coverage = depth_df.iloc[:, 2].mean(skipna=True, numeric_only=True)
+    else:
+        coverage = 0
+        #os.remove(f"{outdir}/depth.txt")
     if os.path.exists(f"{outdir}/mres.vcf") is True:
         os.remove(f"{outdir}/mres.vcf")
     bcftools_cmd = f"bcftools mpileup -f {rrna_seq} {outdir}/23s_aln.sort.bam | bcftools call -mv -Ov -o {outdir}/mres.vcf"
     assists.run_cmd(bcftools_cmd)
 
     if mutation_list != []:
-        mres_df, result_dict = vcf_calc_and_blast_match(f"{outdir}/mres.vcf", mutation_list)
+        mres_df, result_dict = vcf_calc_and_blast_match(f"{outdir}/mres.vcf", mutation_list, coverage)
     else:    
-        result_dict, mres_df = map_calculations(f"{outdir}/mres.vcf")
+        result_dict, mres_df = map_calculations(f"{outdir}/mres.vcf", coverage)
     mres_df.to_csv(f"{outdir}/23s_mres.txt", sep="\t", index=False, header=False) 
     logging.info(f"Any other mutations detected in the 23S rRNA V domain will be written to {outdir}/23s_mres.txt")
     return result_dict
 
-def vcf_calc_and_blast_match(bcftools_vcf, mutation_list):
+def vcf_calc_and_blast_match(bcftools_vcf, mutation_list, coverage):
     positions = None
     with open(bcftools_vcf, "r") as vcf:
         oneline = ""
@@ -79,7 +88,13 @@ def vcf_calc_and_blast_match(bcftools_vcf, mutation_list):
         final_df = pd.DataFrame()
         logging.info(f"Mutations detected in assembly was not detected in mapping.")
     
-    if "A2037G" in mutation_list:
+    if coverage < 10:
+        result_dict = {
+            "Resistance": "Low Coverage!",
+            "Mutation": positions,
+            "Copy No": f"{str(copy_no)} copies",
+        }
+    elif "A2037G" in mutation_list:
         result_dict = {
             "Resistance": "Resistant",
             "Mutation": positions,
@@ -110,47 +125,56 @@ def determine_copy_number(freq):
     else:
         return f"(Freq: {perc}"
 
-def map_calculations(bcftools_vcf):
-    with open(bcftools_vcf, "r") as vcf:
-        oneline = ""
-        lines = vcf.readlines()
-        for line in lines:
-            if not line.startswith("##"):
-                oneline += line
-    
-    vcf_df = pd.read_csv(StringIO(oneline), sep="\t", header=0)
+def map_calculations(bcftools_vcf, coverage):
     mres_df = pd.DataFrame()
-    if vcf_df.empty is False:
-        info_data = vcf_df["INFO"].str.split(";", expand = True)
+    if coverage > 10:
+        with open(bcftools_vcf, "r") as vcf:
+            oneline = ""
+            lines = vcf.readlines()
+            for line in lines:
+                if not line.startswith("##"):
+                    oneline += line
         
-        mres_df['DP4'] = info_data.apply(lambda row: next((cell for cell in row if "DP4=" in cell), None), axis=1).str.lstrip('DP4=')
-        split_columns = mres_df['DP4'].str.split(',', expand=True)
-        split_columns = split_columns.apply(pd.to_numeric, errors='coerce')
-        mres_df['sum_all_four'] = split_columns.sum(axis=1).astype(int)
-        mres_df['sum_last_two'] = split_columns.iloc[:, -2:].sum(axis=1).astype(int)
-        mres_df['FREQ'] = mres_df['sum_last_two'] / mres_df['sum_all_four']
+        vcf_df = pd.read_csv(StringIO(oneline), sep="\t", header=0)
         
-        # extract only V domain
-        vdomain_start, vdomain_end = 1918, 2444
-        mres_df["REFPOSALT"], mres_df["POS"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"], vcf_df["POS"]
-        mres_df = mres_df[(mres_df["POS"] >= vdomain_start) & (mres_df["POS"] <= vdomain_end)]
-        mut_total = len(mres_df.index)
-        copy_no = mres_df["FREQ"].apply(determine_copy_number).to_list()
-        mres_df = mres_df[['REFPOSALT', 'FREQ']]
-        positions = mres_df["REFPOSALT"].to_list()
-        logging.info(f"23S mutation occurs as a {positions}, very likely in {copy_no}")
-        if mres_df["REFPOSALT"].str.contains("A2037G").any():
-            result_dict = {
-                "Resistance": "Resistant",
-                "Mutation": ",".join(positions),
-                "Copy No": ",".join(copy_no)
-            }
-        elif positions != []:
-            result_dict = {
-                "Resistance": "Mutations in 23S rRNA V domain detected",
-                "Mutation": ", ".join(positions),
-                "Copy No": ", ".join(copy_no)
-            }
+        if vcf_df.empty is False:
+            info_data = vcf_df["INFO"].str.split(";", expand = True)
+            
+            mres_df['DP4'] = info_data.apply(lambda row: next((cell for cell in row if "DP4=" in cell), None), axis=1).str.lstrip('DP4=')
+            split_columns = mres_df['DP4'].str.split(',', expand=True)
+            split_columns = split_columns.apply(pd.to_numeric, errors='coerce')
+            mres_df['sum_all_four'] = split_columns.sum(axis=1).astype(int)
+            mres_df['sum_last_two'] = split_columns.iloc[:, -2:].sum(axis=1).astype(int)
+            mres_df['FREQ'] = mres_df['sum_last_two'] / mres_df['sum_all_four']
+            
+            # extract only V domain
+            vdomain_start, vdomain_end = 1918, 2444
+            mres_df["REFPOSALT"], mres_df["POS"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"], vcf_df["POS"]
+            mres_df = mres_df[(mres_df["POS"] >= vdomain_start) & (mres_df["POS"] <= vdomain_end)]
+            mut_total = len(mres_df.index)
+            copy_no = mres_df["FREQ"].apply(determine_copy_number).to_list()
+            mres_df = mres_df[['REFPOSALT', 'FREQ']]
+            positions = mres_df["REFPOSALT"].to_list()
+            logging.info(f"23S mutation occurs as a {positions}, very likely in {copy_no}")
+            if mres_df["REFPOSALT"].str.contains("A2037G").any():
+                result_dict = {
+                    "Resistance": "Resistant",
+                    "Mutation": ",".join(positions),
+                    "Copy No": ",".join(copy_no)
+                }
+            elif positions != []:
+                result_dict = {
+                    "Resistance": "Mutations in 23S rRNA V domain detected",
+                    "Mutation": ", ".join(positions),
+                    "Copy No": ", ".join(copy_no)
+                }
+            else:
+                logging.info(f"No 23S mutations were detected in this sample")
+                result_dict = {
+                    "Resistance": "Susceptible",
+                    "Mutation": "N/A",
+                    "Copy No": "N/A"
+                    }
         else:
             logging.info(f"No 23S mutations were detected in this sample")
             result_dict = {
@@ -158,11 +182,10 @@ def map_calculations(bcftools_vcf):
                 "Mutation": "N/A",
                 "Copy No": "N/A"
                 }
-    else:
-        logging.info(f"No 23S mutations were detected in this sample")
+    else: 
         result_dict = {
-            "Resistance": "Susceptible",
+            "Resistance": "Low Coverage!",
             "Mutation": "N/A",
-            "Copy No": "N/A"
-            }
+            "Copy No": "N/A",
+        } 
     return result_dict, mres_df
